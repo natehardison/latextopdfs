40591a54fe32db3a1fcdc04dfcd0819ead2e88ae
Nate Hardison <natehardison@gmail.com>
2012-05-25 20:50:48 -0400

    Major rewrite, now uses stdin instead of command line args

--- latextopdfs.py
+++ latextopdfs.py
@@ -2,125 +2,122 @@
 
 import argparse
 import csv
+import jinja2
 import os
+import re
+import subprocess
 import sys
-from subprocess import call
 from tempfile import mkdtemp, mkstemp
-from jinja2 import Environment, FileSystemLoader, StrictUndefined, UndefinedError
 
-# Makes one PDF file by passing "substitutions" through "template"
-# and through pdflatex.
-def make_one_pdf(template, substitutions, original_dir):
-    # We're going to throw away this file after we make the PDF
-    texfile, texfilename = mkstemp(dir=os.curdir)
+class PDFGenerator:
+    """Generates PDF files from a jinja2 template"""
+    def __init__(self, template_path):
+        # Split path into dir/name for jinja2
+        template_dir, template_name = os.path.split(os.path.abspath(template_path))
 
-    # Add in the template file directory as a variable so that the template
-    # can locate any resources referenced from it (e.g. image files)
-    substitutions['TemplateDirectory'] = os.path.dirname(template.filename)
+        # Set up the jinja2 environment so that it doesn't get confused with other LaTeX markup
+        # Credit: http://e6h.de/post/11/
+        env = jinja2.Environment(
+            block_start_string = '\BLOCK{',
+            block_end_string = '}',
+            variable_start_string = '\VAR{',
+            variable_end_string = '}',
+            comment_start_string = '\#{',
+            comment_end_string = '}',
+            line_statement_prefix = '%-',
+            line_comment_prefix = '%#',
+            trim_blocks = True,
+            autoescape = False,
+            undefined = jinja2.StrictUndefined,
+            loader = jinja2.FileSystemLoader(template_dir)
+        )
 
-    # Pass the TeX template through jinja2 templating engine and into the temp file
-    # Raises an error if there's an undefined variable in the template
-    try:
-        os.write(texfile, template.render(substitutions).encode('utf8'))
-    except UndefinedError as error:
-        print "Error processing template: %s." % error.message
-        sys.exit(0)
-        
-    os.close(texfile)
-    
-    call(['pdflatex', texfilename])
+        # Provide a filter to allow templates to resolve paths relative to their
+        # own location in the filesystem
+        env.filters['abspath'] = lambda path: os.path.join(template_dir, path)
 
-    # There's got to be a better way to do this...
-    if 'Destination' in substitutions:
-        destination = os.path.expanduser(substitutions['Destination'])
-    else:
-        destination = template.name
+        # Load the template file into the jinja2 engine
+        try:
+            self._template = env.get_template(template_name)
+        except jinja2.exceptions.TemplateNotFound:
+            raise PDFGenerator.Error("%s: No such file or directory" % template_path)
+        except jinja2.exceptions.TemplateSyntaxError as e:
+            raise PDFGenerator.Error("%s:%d: error: %s" % (e.filename, e.lineno, e.message))
 
-    if not os.path.isabs(destination):
-        destination = os.path.normpath(os.path.join(original_dir, destination))
+    def generate_pdf(self, substitutions, destination):
+        """Makes one PDF file by passing "substitutions" through "template" and
+           through pdflatex. Saves the resulting PDF as "destination"."""
+        # Pass the LaTeX template through jinja2 templating engine and into the temp file
+        # Raises an error if there's an undefined variable in the template
+        try:
+            tex_source = self._template.render(substitutions).encode('utf8')
+        except jinja2.UndefinedError as e:
+            raise PDFGenerator.Error("%s: error: %s" % (self._template.filename, e.message))
 
-    destination = os.path.splitext(destination)[0]
+        # We're going to throw away this file after we make the PDF
+        texfile, texfilename = mkstemp(dir=os.curdir)
+        os.write(texfile, tex_source)
+        os.close(texfile)
 
-    os.rename(texfilename + '.pdf', destination + '.pdf')
+        # This is a hack because Python < 3 doesn't support subprocess.DEVNULL
+        with open("/dev/null", 'w') as devnull:
+            try:
+                subprocess.check_call(["pdflatex", "-halt-on-error", texfilename], stdout=devnull)
+            except subprocess.CalledProcessError as e:
+                os.rename(texfilename + ".log", destination + ".log")
+                error_message = "Command 'pdflatex' returned non-zero exit code %d." % e.returncode
+                error_message += " Error log saved as %s" % (destination + ".log")
+                raise PDFGenerator.Error(error_message)
+            else:
+                os.rename(texfilename + ".pdf", destination + ".pdf")
+            finally:
+                os.remove(texfilename)
+                if os.path.exists(texfilename + ".aux"):
+                    os.remove(texfilename + ".aux")
+                if os.path.exists(texfilename + ".log"):
+                    os.remove(texfilename + ".log")
 
-    # These are just trash now
-    os.remove(texfilename)
-    os.remove(texfilename + '.aux')
-    os.remove(texfilename + '.log')
+    class Error(Exception):
+        def __init__(self, message):
+            self.message = message
+        def __str__(self):
+            return repr(self.message)
+    # end class Error
+# end class PDFGenerator
 
-    return destination
+def csv_substitutions_reader(substitutions_file):
+    csv_reader = csv.DictReader(substitutions_file)
+    for row in csv_reader:
+        yield row
 
-
-def load_template(template_path):
-    # Split path into name/dir for jinja2
-    template_name = os.path.basename(template_path)
-    template_dir = os.path.dirname(template_path)
-
-    # Set up the jinja2 environment so that it doesn't get
-    # confused with other LaTeX markup
-    # Credit: http://e6h.de/post/11/
-    env = Environment(
-        block_start_string = '\BLOCK{',
-        block_end_string = '}',
-        variable_start_string = '\VAR{',
-        variable_end_string = '}',
-        comment_start_string = '\#{',
-        comment_end_string = '}',
-        line_statement_prefix = '%-',
-        line_comment_prefix = '%#',
-        trim_blocks = True,
-        autoescape = False,
-        undefined = StrictUndefined,
-        loader = FileSystemLoader(template_dir)
-    )
-
-    # Load the template file into the jinja2 engine
-    return env.get_template(template_name)
-
-
-def parse_substitution_args(sub_args):
-    # Try parsing the args first as a filename
-    if (len(sub_args) == 1 and
-        os.path.exists(sub_args[0]) and
-        os.path.isfile(sub_args[0])):
-        return (sub_args[0], True)
-    
-    # Otherwise, assume that the user passed in a bunch of key=value pairs    
-    try:
-        return (dict([arg.split('=') for arg in sub_args]), False)
-    except:
-        print "Invalid substitutions \"%s\" Please use valid key=value format." % ' '.join(sub_args)
-        sys.exit(1)
-
-def validate_template_file(template):
-    return (os.path.splitext(template)[1] == '.tex' and
-            os.path.exists(template) and
-            os.path.isfile(template))
+def key_value_substitutions_reader(substitutions_file):
+    r = re.compile('("[^"]+"|[^ =]+)\s*=\s*("[^"]*"|[^ ]*)')
+    for line_number, line in enumerate(substitutions_file):
+        try:
+            substitutions = dict([(k.strip('"'), v.strip('"')) for k, v in r.findall(line)])
+        except ValueError:
+            print >> sys.stderr, "%s:%d: error: Invalid key=value format %s" % (substitutions_file.name, line_number, line)
+        else:
+            yield substitutions
 
 def main():
     parser = argparse.ArgumentParser(description='Generate PDF files from LaTeX templates')
     parser.add_argument('template', help='the LaTeX template to use')
-    parser.add_argument('substitutions', nargs='+',
-        help='the list of substitutions, which can be given either in a CSV\
-              file or on the command line as key=value pairs. Use\
-              Destination=path/to/file to specify the destination of the output\
-              file relative to the current directory')
+    parser.add_argument('substitutions', nargs='?', type=argparse.FileType('r'),
+        default=sys.stdin, help='the list of substitutions, which can be given\
+                                 either in a CSV file or via stdin as key=value\
+                                 pairs. Use OutputFile to designate the desired\
+                                 destination for each PDF file.')
     args = parser.parse_args()
 
-    # Ensure we got a valid template file (should have .tex extension), and it
-    # should actually be a file.
-    if not validate_template_file(args.template):
-        print "Invalid template file \"%s.\" Template must be a valid LaTeX file with the .tex extension." % args.template
+    try:
+        pdf_generator = PDFGenerator(args.template)
+    except PDFGenerator.Error as e:
+        print >> sys.stderr, e.message
         sys.exit(1)
 
-    # User might have passed in a relative path for template and we need its
-    # parent dir anyway for the jinja2 engine
-    template = load_template(os.path.abspath(args.template))
-
-    # If use_fule = True, then substitutions will be a CSV file containing the
-    # substitutions. Otherwise, substitutions is a dict containing the args
-    # passed in on the command line.
-    substitutions, use_file = parse_substitution_args(args.substitutions)
+    # In case the user didn't put in a default destination for each PDF file
+    default_destination = os.path.splitext(os.path.basename(args.template))[0]
 
     # Make a temp folder to do our dirty work; remember current dir so we can
     # get back here later
@@ -128,19 +125,30 @@ def main():
     tmp_folder = mkdtemp()
     os.chdir(tmp_folder)
 
-    if use_file:
-        with open(substitutions, 'rU') as subs_file:
-            subs_file_reader = csv.DictReader(subs_file)
-            for subs_row in subs_file_reader:
-                destination = make_one_pdf(template, subs_row, original_dir)
+    if args.substitutions == sys.stdin:
+        print "Enter substitutions as key=value pairs. Each line should specify\
+ all of the substitutions necessary to create one PDF file."
+
+    if os.path.splitext(args.substitutions.name)[1] == ".csv":
+        substitutions_reader = csv_substitutions_reader(args.substitutions)
     else:
-        destination = make_one_pdf(template, substitutions, original_dir)
+        substitutions_reader = key_value_substitutions_reader(args.substitutions)
+
+    for line_number, substitutions in enumerate(substitutions_reader):
+        if "OutputFile" in substitutions:
+            destination = os.path.splitext(os.path.expanduser(substitutions["OutputFile"]))[0]
+        else:
+            destination = default_destination + "_%d" % line_number
+        destination = os.path.normpath(os.path.join(original_dir, destination))
+        try:
+            pdf_generator.generate_pdf(substitutions, destination)
+        except PDFGenerator.Error as e:
+            print >> sys.stderr, e.message + " (from substitutions line %d)" % line_number
+        else:
+            print "Created %s.pdf" % destination
 
     os.chdir(original_dir)
     os.rmdir(tmp_folder)
-
-    print "File(s) stored in %s." % os.path.dirname(destination)    
-
 
 if __name__ == '__main__':
     main()
